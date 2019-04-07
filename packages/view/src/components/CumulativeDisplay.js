199b734760082e8d0d67a5f5af4fc61ef67401a5
vyshakg <vyshak.g.gowda@gmail.com>
2019-04-07 22:15:44 +0530

    added cumulative diaplay

---
+++ packages/view/src/components/CumulativeDisplay.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { CumulativeWrapper } from "../ui/StyledComponents";
+
+function cumulativeDisplay() {
+  return <CumulativeWrapper>23333</CumulativeWrapper>;
+}
+
+export default cumulativeDisplay;
